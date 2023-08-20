b8d1c90a2db47c5da9566591773c46f6bd00cede
Juan José Garzón Luján <juanjogarzon969@gmail.com>
2023-08-21 01:47:26 +0200

    feat: GET methods

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const express = require('express')
-
+const movies = require('./movies.json')
 const app = express()
 app.disable('x-powered-by')
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Hola mundo' })
+app.get('/movies', (req, res) => {
+  const { genre } = req.query
+
+  if (genre) {
+    const filteredMovies = movies.filter(
+      movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase())
+    )
+
+    return res.json(filteredMovies)
+  }
+  res.json(movies)
+})
+
+app.get('/movies/:id', (req, res) => {
+  const { id } = req.params
+  const movie = movies.find(movie => movie.id === id)
+
+  if (movie) return res.json(movie)
+
+  res.status(404).json({ message: 'Movie not found' })
 })
 
 const PORT = process.env.PORT ?? 1234
