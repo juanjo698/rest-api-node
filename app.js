edfa32eca0a5f97e49c05ce852f3a3e0f2c82ead
Juan José Garzón Luján <juanjogarzon969@gmail.com>
2023-08-21 03:02:11 +0200

    feat: resolve cors problem

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 const express = require('express')
 const movies = require('./movies.json')
 const crypto = require('node:crypto')
+const cors = require('cors')
 const { validateMovie, validatePartialMovie } = require('./schemas/movies')
 
 const app = express()
 app.disable('x-powered-by')
 app.use(express.json())
+app.use(cors({
+  origin: (origin, callback) => {
+    const ACCEPTED_ORIGINS = [
+      'http://localhost:8080'
+    ]
+
+    if (ACCEPTED_ORIGINS.includes(origin)) {
+      return callback(null, true)
+    }
+
+    if (!origin) {
+      return callback(null, true)
+    }
+
+    return callback(new Error('Not allowed by CORS'))
+  }
+}))
 
 app.get('/movies', (req, res) => {
   const { genre } = req.query
@@ -69,6 +87,19 @@ app.patch('/movies/:id', (req, res) => {
   }
 
   return res.json(updatedMovie)
+})
+
+app.delete('/movies/:id', (req, res) => {
+  const { id } = req.params
+  const movieIndex = movies.findIndex(movie => movie.id === id)
+
+  if (movieIndex === -1) {
+    return res.status(404).json({ message: 'Movie not found' })
+  }
+
+  movies.splice(movieIndex, 1)
+
+  return res.json({ message: 'Movie deleted' })
 })
 
 const PORT = process.env.PORT ?? 1234
