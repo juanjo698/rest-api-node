6f56dc1c01ca5a2d8ceea9582f09ffeeb03f9599
Juan José Garzón Luján <juanjogarzon969@gmail.com>
2023-08-21 02:09:05 +0200

    feat: POST a new movie

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const movies = require('./movies.json')
+const crypto = require('node:crypto')
 const app = express()
 app.disable('x-powered-by')
+app.use(express.json())
 
 app.get('/movies', (req, res) => {
   const { genre } = req.query
@@ -14,6 +16,34 @@ app.get('/movies', (req, res) => {
     return res.json(filteredMovies)
   }
   res.json(movies)
+})
+
+app.post('/movies', (req, res) => {
+  const {
+    title,
+    genre,
+    year,
+    director,
+    duration,
+    rate,
+    poster
+  } = req.body
+
+  const newMovie = {
+    id: crypto.randomUUID(),
+    title,
+    genre,
+    year,
+    director,
+    duration,
+    rate: rate ?? 0,
+    poster
+  }
+
+  // this is not REST, DB implementation missing
+  movies.push(newMovie)
+
+  res.status(201).json(newMovie)
 })
 
 app.get('/movies/:id', (req, res) => {
